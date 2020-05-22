9c3396d463e6b79177c8eece6d1de2962bf0a82e
Juan Pablo Villaplana <jpvillaplana@gmail.com>
2020-05-21 21:20:14 -0600

    Palindromo2

--- palindromo.py
+++ palindromo.py
@@ -10,12 +10,18 @@ def palindrome(word):
         return True
     return False
 
+def palindrome2(word):
+    reversed_word = word[::-1]
+
+    if reversed_word == word:
+        return True
+    return False
 
 
 if __name__ == "__main__":
     word = str(input('Escribe una palabra: '))
 
-    result = palindrome(word)
+    result = palindrome2(word)
 
     if result is True:
         print('La palabra {} es una palidromo'.format(word))
